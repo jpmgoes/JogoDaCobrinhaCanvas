9772a826ae73b77cadc761bcf1252c13457797c2
JPMouraG <jp.mouragoes@gmail.com>
2021-11-28 04:06:08 -0300

    commited

--- cobra.js
+++ cobra.js
@@ -1,3 +1,40 @@
+// tela config
+let tela;
+let ctx;
+let ATRASO = 120;
+const TAMANHO_PONTO = 30;
+const ALEATORIO_MAXIMO = 25;
+const C_ALTURA = 900;
+const C_LARGURA = C_ALTURA;
+const centro = {
+  lado: C_LARGURA / 2,
+  altura: C_ALTURA / 2,
+};
+
+class AddNoJogo {
+  constructor(qnt = 0, imgQnt = 1) {
+    this.img = Array(imgQnt).fill(null);
+    this.audio = null;
+    this.x = [];
+    this.y = [];
+    this.qnt = qnt;
+    this.invisivel = -100;
+  }
+  carregarImg(imgName, type) {
+    this.img.forEach((_, i) => {
+      this.img[+i] = new Image();
+      this.img[+i].src = `./img/${imgName}${+i + 1}.${type}`;
+    });
+  }
+  fazerDesenho(cb) {
+    cb();
+  }
+}
+
+let comida = new AddNoJogo(15);
+let obstaculo = new AddNoJogo(10, 2);
+let cobra = new AddNoJogo(3, 2);
+
 // cronometro
 let countCronometro = {
   min: 1,
@@ -5,23 +42,14 @@ let countCronometro = {
 };
 let minFormatado = countCronometro.min.toString().padStart(2, "0");
 let secFormatado = countCronometro.sec.toString().padStart(2, "0");
-// img
-let cabeca;
-let maca;
-let bola;
-let obstaculo = Array(2).fill(null);
+document.querySelector(
+  ".cronometro"
+).textContent = `${minFormatado}:${secFormatado}`;
 
 // audio
-let macaAudio;
-let obstaculoAudio;
 let bgm;
 let gameOver;
 let wins;
-
-// obstaculo config
-const obstaculoQnt = 10;
-const obstaculo_x = [];
-const obstaculo_y = [];
 
 // localStorage
 let players = localStorage.getItem("players")
@@ -34,15 +62,9 @@ let players = localStorage.getItem("players")
 if (!localStorage.getItem("players"))
   localStorage.setItem("players", JSON.stringify(players));
 
-let pontos;
-let vida = 5;
-
 //alimento
-let maca_x = [];
-let maca_y = [];
-const macaQnt = 15;
-const macaInv = -100;
 let countComidasPraVida = 0;
+let vida = 5;
 
 // start game config
 let randomStart = Math.round(Math.random() * 2) % 2;
@@ -52,15 +74,6 @@ let paraCima = false;
 let paraBaixo = false;
 let noJogo = true;
 
-// tela config
-let tela;
-let ctx;
-let ATRASO = 120;
-const TAMANHO_PONTO = 30;
-const ALEATORIO_MAXIMO = 25;
-const C_ALTURA = 900;
-const C_LARGURA = C_ALTURA;
-
 // move
 const TECLA_ESQUERDA = 37;
 const TECLA_DIREITA = 39;
@@ -68,8 +81,6 @@ const TECLA_ACIMA = 38;
 const TECLA_ABAIXO = 40;
 
 // cobra config
-const cobraX = [];
-const cobraY = [];
 
 onkeydown = verificarTecla; // Define função chamada ao se pressionar uma tecla
 
@@ -93,79 +104,68 @@ function iniciar() {
     bgm.loop = true;
     criarCobra();
     htmlVidaChange();
-    addNoJogo(obstaculo_x, obstaculo_y, obstaculoQnt, maca_x);
-    addNoJogo(maca_x, maca_y, macaQnt, obstaculo_x);
+    addNoJogo(obstaculo, obstaculo.qnt, comida.x);
+    addNoJogo(comida, comida.qnt, obstaculo.x);
   }, ATRASO);
 
   setTimeout("cicloDeJogo()", ATRASO);
 }
 
 function carregarAudio() {
-  obstaculoAudio = new Audio("./audio/Roblox Death Sound.mp3");
-  macaAudio = new Audio("./audio/marioMoeda.mp3");
+  obstaculo.audio = new Audio("./audio/Roblox Death Sound.mp3");
+  comida.audio = new Audio("./audio/marioMoeda.mp3");
   bgm = new Audio("./audio/Sweden.mp3");
   gameOver = new Audio("./audio/gameOver.mp3");
   wins = new Audio("./audio/marioWins.mp3");
 }
 
 function carregarImagens() {
-  cabeca = new Image();
-  cabeca.src = "./img/cabeca.svg";
-
-  bola = new Image();
-  bola.src = "./img/ponto.svg";
-
-  maca = new Image();
-  maca.src = "./img/maca.svg";
-
-  for (let i in obstaculo) {
-    obstaculo[+i] = new Image();
-    obstaculo[+i].src = `./img/obstaculo${+i + 1}.svg`;
-  }
+  cobra.carregarImg("cobra", "svg");
+  comida.carregarImg("comida", "svg");
+  obstaculo.carregarImg("obstaculo", "svg");
 }
 
 function criarCobra() {
-  pontos = 3;
-
   const posAleatoria = Math.random() * C_LARGURA - TAMANHO_PONTO;
   const a = posAleatoria - (posAleatoria % TAMANHO_PONTO);
 
-  for (var z = 0; z < pontos; z++) {
-    cobraX[z] = a - z * TAMANHO_PONTO;
-    cobraY[z] = a;
+  for (var z = 0; z < cobra.qnt; z++) {
+    cobra.x[z] = a - z * TAMANHO_PONTO;
+    cobra.y[z] = a;
   }
 }
 
-function addNoJogo(arrX, arrY, qnt, ...arrColision) {
+function addNoJogo(obj, qnt, ...arrColision) {
   for (let i = 0; i < qnt; i++) {
-    while (!arrX[i] || arrY[i] === cobraY[0]) {
-      // se já tiver sido comida, verificar se já foi spawnado
+    while (!obj.x[i] || obj.y[i] === cobra.x[0]) {
+      // garantir que vai pegar todos os valores e nao vai ser na mesma linha da cobra
       while (true) {
         // evitar mesma posicao
-        var r = Math.floor(Math.random() * ALEATORIO_MAXIMO);
-
+        const r = Math.floor(Math.random() * ALEATORIO_MAXIMO);
         const value = r * TAMANHO_PONTO;
 
-        if (!arrX.find((a) => a === value)) {
+        if (!obj.x.find((a) => a === value)) {
           for (let arr of arrColision)
-            if (!arr.find((a) => a === value)) arrX[i] = value;
+            if (!arr.find((a) => a === value)) obj.x[i] = value;
         } else break;
       }
-      r = Math.floor(Math.random() * ALEATORIO_MAXIMO);
-      arrY[i] = r * TAMANHO_PONTO;
-      if (arrX.length === qnt) break;
+      const r = Math.floor(Math.random() * ALEATORIO_MAXIMO);
+      obj.y[i] = r * TAMANHO_PONTO;
+      if (obj.x.length === qnt) break;
     }
   }
-  return [arrX, arrY];
+  return [obj.x, obj.y];
 }
 
 function cicloDeJogo() {
   if (noJogo) {
     verificarColisao();
     mover();
+    ctx.clearRect(0, 0, C_LARGURA, C_ALTURA);
+    ctx.fillRect(0, 0, C_LARGURA, C_ALTURA);
     fazerDesenho();
     setTimeout("cicloDeJogo()", ATRASO);
-  }
+  } else fimDeJogo();
 }
 
 function htmlVidaChange() {
@@ -173,11 +173,11 @@ function htmlVidaChange() {
   vidaElement.textContent = vida.toString().padStart(2, "0");
 }
 
-function aoComerMaca(index) {
-  macaAudio.play();
-  pontos++;
-  maca_x[index] = macaInv;
-  maca_y[index] = macaInv;
+function aoComerComida(index) {
+  comida.audio.play();
+  cobra.qnt++;
+  comida.x[index] = comida.invisivel;
+  comida.y[index] = comida.invisivel;
   ATRASO -= 2;
   countComidasPraVida++;
 
@@ -189,91 +189,98 @@ function aoComerMaca(index) {
   if (ATRASO >= 90) ATRASO = 50;
 }
 
-function macaDpsDeComerTd() {
-  if (verificarQntdeMaca()) noJogo = false;
+function comidaDpsDeComerTd() {
+  if (verificarQntdeComida()) noJogo = false;
 }
 
 function aoColidirComObj(index) {
   vida--;
   htmlVidaChange();
-  obstaculoAudio.play();
-  obstaculo_x[index] = macaInv;
+  obstaculo.audio.play();
+  obstaculo.x[index] = obstaculo.invisivel;
   if (vida === 0) {
     vida = 0;
     noJogo = false;
   }
 }
 
-function objColision(arrX, arrY, qnt, ...cb) {
-  for (let i = 0; i < qnt; i++)
-    if (cobraX[0] == arrX[i] && cobraY[0] == arrY[i]) cb[0](i);
+function objColision(obj, ...cb) {
+  for (let i = 0; i < obj.qnt; i++)
+    if (cobra.x[0] == obj.x[i] && cobra.y[0] == obj.y[i]) cb[0](i);
 
   cb = cb.slice(1);
   cb.forEach((fn) => fn());
 }
 
-function verificarQntdeMaca() {
+function verificarQntdeComida() {
   let count = 0;
-  for (let i = 0; i < macaQnt; i++) if (maca_x[i] === macaInv) count++;
-  return count === maca_x.length;
+  for (let i = 0; i < comida.qnt; i++)
+    if (comida.x[i] === comida.invisivel) count++;
+  return count === comida.x.length;
 }
 
 function ajustarPosicaoDaCobra(a, b) {
-  cobraX[0] = a;
-  cobraY[0] = b;
+  cobra.x[0] = a;
+  cobra.y[0] = b;
 }
 
 function verificarColisao() {
-  for (var z = pontos; z > 0; z--)
-    if (z > 4 && cobraX[0] == cobraX[z] && cobraY[0] == cobraY[z])
+  for (var z = cobra.qnt; z > 0; z--)
+    if (z > 4 && cobra.x[0] == cobra.x[z] && cobra.y[0] == cobra.y[z])
       noJogo = false;
 
   // baixo
-  if (cobraY[0] >= C_ALTURA) ajustarPosicaoDaCobra(cobraX[0], 0);
+  if (cobra.y[0] >= C_ALTURA) ajustarPosicaoDaCobra(cobra.x[0], 0);
 
   // cima
-  if (cobraY[0] < 0) ajustarPosicaoDaCobra(cobraX[0], C_ALTURA);
+  if (cobra.y[0] < 0) ajustarPosicaoDaCobra(cobra.x[0], C_ALTURA);
 
   // direita
-  if (cobraX[0] >= C_LARGURA) ajustarPosicaoDaCobra(0, cobraY[0]);
+  if (cobra.x[0] >= C_LARGURA) ajustarPosicaoDaCobra(0, cobra.y[0]);
 
   // esquerda
-  if (cobraX[0] < 0) ajustarPosicaoDaCobra(C_LARGURA, cobraY[0]);
+  if (cobra.x[0] < 0) ajustarPosicaoDaCobra(C_LARGURA, cobra.y[0]);
 
-  objColision(obstaculo_x, obstaculo_y, obstaculoQnt, aoColidirComObj);
-  objColision(maca_x, maca_y, macaQnt, aoComerMaca, macaDpsDeComerTd);
+  objColision(obstaculo, aoColidirComObj);
+  objColision(comida, aoComerComida, comidaDpsDeComerTd);
 }
 
 function mover() {
-  for (var z = pontos; z > 0; z--) {
-    cobraX[z] = cobraX[z - 1];
-    cobraY[z] = cobraY[z - 1];
+  for (var z = cobra.qnt; z > 0; z--) {
+    cobra.x[z] = cobra.x[z - 1];
+    cobra.y[z] = cobra.y[z - 1];
   }
 
-  if (paraEsquerda) cobraX[0] -= TAMANHO_PONTO;
+  if (paraEsquerda) cobra.x[0] -= TAMANHO_PONTO;
 
-  if (paraDireita) cobraX[0] += TAMANHO_PONTO;
+  if (paraDireita) cobra.x[0] += TAMANHO_PONTO;
 
-  if (paraCima) cobraY[0] -= TAMANHO_PONTO;
+  if (paraCima) cobra.y[0] -= TAMANHO_PONTO;
 
-  if (paraBaixo) cobraY[0] += TAMANHO_PONTO;
+  if (paraBaixo) cobra.y[0] += TAMANHO_PONTO;
 }
 
 function fazerDesenho() {
-  ctx.clearRect(0, 0, C_LARGURA, C_ALTURA);
-  ctx.fillRect(0, 0, C_LARGURA, C_ALTURA);
-
   if (noJogo) {
-    for (let i = 0; i < obstaculoQnt; i++)
-      if (i > 4) ctx.drawImage(obstaculo[0], obstaculo_x[i], obstaculo_y[i]);
-      else ctx.drawImage(obstaculo[1], obstaculo_x[i], obstaculo_y[i]);
-    for (let i = 0; i < macaQnt; i++) ctx.drawImage(maca, maca_x[i], maca_y[i]);
-    for (var z = 0; z < pontos; z++) {
-      if (z < Math.floor(cobraX.length / 2) - 1)
-        ctx.drawImage(cabeca, cobraX[z], cobraY[z]);
-      else ctx.drawImage(bola, cobraX[z], cobraY[z]);
-    }
-  } else fimDeJogo();
+    const objNoJogo = [comida, obstaculo, cobra];
+
+    obstaculo.fn = (i) => {
+      if (i > 4)
+        ctx.drawImage(obstaculo.img[0], obstaculo.x[i], obstaculo.y[i]);
+      else ctx.drawImage(obstaculo.img[1], obstaculo.x[i], obstaculo.y[i]);
+    };
+
+    comida.fn = (i) => ctx.drawImage(comida.img[0], comida.x[i], comida.y[i]);
+
+    cobra.fn = (i) => {
+      if (i < Math.floor(cobra.x.length / 2) - 1)
+        ctx.drawImage(cobra.img[0], cobra.x[i], cobra.y[i]);
+      else ctx.drawImage(cobra.img[1], cobra.x[i], cobra.y[i]);
+    };
+
+    for (let obj of objNoJogo)
+      for (let i = 0; i < obj.qnt; i++) obj.fazerDesenho(() => obj.fn(i));
+  }
 }
 
 function addPlayer() {
@@ -287,7 +294,7 @@ function addPlayer() {
     nome = "NoN"; // No Name
   }
   players.nome.push(nome);
-  players.ponto.push(pontos - 3);
+  players.ponto.push(cobra.qnt - 3);
   players.time.push(`${minFormatado}:${secFormatado}`);
   players = sortBest5players(players);
   localStorage.setItem("players", JSON.stringify(players));
@@ -318,13 +325,7 @@ function permuta(arr, indexI, indexJ) {
 
 function fimDeJogo() {
   addPlayer();
-  const centro = {
-    lado: C_LARGURA / 2,
-    altura: C_ALTURA / 2,
-  };
-
   let myFont = new FontFace("PressStart2P", "url(./font/pressstart2p.ttf)");
-
   myFont.load().then((font) => {
     document.fonts.add(font);
     ctx.fillStyle = "white";
@@ -334,7 +335,7 @@ function fimDeJogo() {
 
     bgm.pause();
 
-    if (!verificarQntdeMaca()) {
+    if (!verificarQntdeComida()) {
       ctx.fillText("DEU MOLE!! 😭😭💀💀", centro.lado, centro.altura - 132);
       gameOver.play();
       setInterval(showPlayers, 3800);
@@ -342,50 +343,50 @@ function fimDeJogo() {
     }
 
     ctx.fillText(
-      `${pontos - 3} PONTOS???😳😳😳`,
+      `${cobra.qnt - 3} PONTOS???😳😳😳`,
       centro.lado,
       centro.altura - 164
     );
     ctx.fillText("BRABO!!🥵🥵🥵", centro.lado, centro.altura - 100);
     wins.play();
     setInterval(showPlayers, 7600);
-
-    function showPlayers() {
-      let countAltura = 64;
-      let countTempo = 0;
-
-      ctx.fillText("NICK PT TIME", centro.lado, centro.altura + countAltura);
-
-      countAltura += 64;
-
-      for (let index = 0; index < 5; index++) {
-        const numIndex = +index;
-        const posicao = +index + 1;
-
-        const playersNome = players.nome[numIndex]
-          ? players.nome[numIndex].padStart(3, " ")
-          : "???";
-        let playersPonto = players.ponto[numIndex]
-          ? players.ponto[numIndex].toString().padStart(2, "0")
-          : "??";
-        if (playersNome !== "???" && playersPonto === "??") playersPonto = "00";
-        const playersTime = players.time[numIndex]
-          ? players.time[numIndex]
-          : "??:??";
-
-        setTimeout(() => {
-          ctx.fillText(
-            `${posicao} ${playersNome} ${playersPonto} ${playersTime}`,
-            centro.lado,
-            centro.altura + countAltura
-          );
-          countAltura += 40;
-        }, countTempo * numIndex);
-
-        countTempo = 500;
-      }
-    }
   });
+}
+
+function showPlayers() {
+  let countAltura = 64;
+  let countTempo = 0;
+
+  ctx.fillText("NICK PT TIME", centro.lado, centro.altura + countAltura);
+
+  countAltura += 64;
+
+  for (let index = 0; index < 5; index++) {
+    const numIndex = +index;
+    const posicao = +index + 1;
+
+    const playersNome = players.nome[numIndex]
+      ? players.nome[numIndex].padStart(3, " ")
+      : "???";
+    let playersPonto = players.ponto[numIndex]
+      ? players.ponto[numIndex].toString().padStart(2, "0")
+      : "??";
+    if (playersNome !== "???" && playersPonto === "??") playersPonto = "00";
+    const playersTime = players.time[numIndex]
+      ? players.time[numIndex]
+      : "??:??";
+
+    setTimeout(() => {
+      ctx.fillText(
+        `${posicao} ${playersNome} ${playersPonto} ${playersTime}`,
+        centro.lado,
+        centro.altura + countAltura
+      );
+      countAltura += 40;
+    }, countTempo * numIndex);
+
+    countTempo = 500;
+  }
 }
 
 function verificarTecla(e) {
