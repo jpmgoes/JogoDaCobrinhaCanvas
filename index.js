5c0686f1c18550abd1c53cb368ee4bf14a4feff9
JPMouraG <jp.mouragoes@gmail.com>
2021-11-28 14:13:42 -0300

    commited

---
+++ index.js
@@ -0,0 +1,71 @@
+// tela config
+let tela;
+let ctx;
+let ATRASO = 120;
+const TAMANHO_PONTO = 30;
+const ALEATORIO_MAXIMO = 25;
+const C_ALTURA = 900;
+const C_LARGURA = C_ALTURA;
+const centro = {
+  lado: C_LARGURA / 2,
+  altura: C_ALTURA / 2,
+};
+
+let comida = new ObjDoJogo(15);
+comida.addNoJogo = addNoJogo;
+comida.objColision = objColision;
+let obstaculo = new ObjDoJogo(10, 2);
+obstaculo.addNoJogo = addNoJogo;
+obstaculo.objColision = objColision;
+
+let cobra = new ObjDoJogo(3, 2);
+cobra.addNoJogo = criarCobra;
+
+// cronometro
+let countCronometro = {
+  min: 1,
+  sec: 20,
+};
+let minFormatado = countCronometro.min.toString().padStart(2, "0");
+let secFormatado = countCronometro.sec.toString().padStart(2, "0");
+document.querySelector(
+  ".cronometro"
+).textContent = `${minFormatado}:${secFormatado}`;
+
+// audio
+let bgm;
+let gameOver;
+let wins;
+
+// localStorage
+let players = localStorage.getItem("players")
+  ? JSON.parse(localStorage.getItem("players"))
+  : {
+      nome: [],
+      ponto: [],
+      time: [],
+    };
+if (!localStorage.getItem("players"))
+  localStorage.setItem("players", JSON.stringify(players));
+
+//alimento
+let countComidasPraVida = 0;
+let vida = 5;
+
+// start game config
+let randomStart = Math.round(Math.random() * 2) % 2;
+let paraEsquerda = randomStart ? 1 : 0;
+let paraDireita = randomStart ? 0 : 1;
+let paraCima = false;
+let paraBaixo = false;
+let noJogo = true;
+
+// move
+const TECLA_ESQUERDA = 37;
+const TECLA_DIREITA = 39;
+const TECLA_ACIMA = 38;
+const TECLA_ABAIXO = 40;
+
+onkeydown = verificarTecla; // Define função chamada ao se pressionar uma tecla
+
+iniciar();
---
+++ script/canvasFn.js
@@ -0,0 +1,169 @@
+function iniciar() {
+  tela = document.getElementById("tela");
+  ctx = tela.getContext("2d");
+
+  ctx.fillStyle = "#F24E1F";
+  ctx.fillRect(0, 0, C_LARGURA, C_ALTURA);
+
+  carregarImagens();
+  carregarAudio();
+  cronometroScreen();
+
+  setTimeout(() => {
+    bgm.play();
+    bgm.loop = true;
+    cobra.addNoJogo();
+    htmlVidaChange();
+    obstaculo.addNoJogo(comida);
+    comida.addNoJogo(obstaculo);
+  }, ATRASO);
+
+  setTimeout("cicloDeJogo()", ATRASO);
+}
+
+function fazerDesenho() {
+  if (noJogo) {
+    const objNoJogo = [comida, obstaculo, cobra];
+
+    obstaculo.fn = (i) => {
+      if (i > 4)
+        ctx.drawImage(obstaculo.img[0], obstaculo.x[i], obstaculo.y[i]);
+      else ctx.drawImage(obstaculo.img[1], obstaculo.x[i], obstaculo.y[i]);
+    };
+
+    comida.fn = (i) => ctx.drawImage(comida.img[0], comida.x[i], comida.y[i]);
+
+    cobra.fn = (i) => {
+      if (i < Math.floor(cobra.x.length / 2) - 1)
+        ctx.drawImage(cobra.img[0], cobra.x[i], cobra.y[i]);
+      else ctx.drawImage(cobra.img[1], cobra.x[i], cobra.y[i]);
+    };
+
+    for (let obj of objNoJogo) for (let i = 0; i < obj.qnt; i++) obj.fn(i);
+  }
+}
+
+function addPlayer() {
+  let nome;
+  try {
+    nome = prompt("Qual teu nome? bota só 3 letras ai 🙈🙈🙈", "Ano").slice(
+      0,
+      3
+    );
+  } catch {
+    nome = "NoN"; // No Name
+  }
+  players.nome.push(nome);
+  players.ponto.push(cobra.qnt - 3);
+  players.time.push(`${minFormatado}:${secFormatado}`);
+  players = sortBest5players(players);
+  localStorage.setItem("players", JSON.stringify(players));
+}
+
+function fimDeJogo() {
+  addPlayer();
+  let myFont = new FontFace("PressStart2P", "url(./font/pressstart2p.ttf)");
+  myFont.load().then((font) => {
+    document.fonts.add(font);
+    ctx.fillStyle = "white";
+    ctx.textBaseline = "middle";
+    ctx.textAlign = "center";
+    ctx.font = `normal bold 32px PressStart2P`;
+
+    bgm.pause();
+
+    if (!verificarQntdeComida()) {
+      ctx.fillText("DEU MOLE!! 😭😭💀💀", centro.lado, centro.altura - 132);
+      gameOver.play();
+      setInterval(showPlayers, 3800);
+      return;
+    }
+
+    ctx.fillText(
+      `${cobra.qnt - 3} PONTOS???😳😳😳`,
+      centro.lado,
+      centro.altura - 164
+    );
+    ctx.fillText("BRABO!!🥵🥵🥵", centro.lado, centro.altura - 100);
+    wins.play();
+    setInterval(showPlayers, 7600);
+  });
+}
+
+function showPlayers() {
+  let countAltura = 64;
+  let countTempo = 0;
+
+  ctx.fillText("NICK PT TIME", centro.lado, centro.altura + countAltura);
+
+  countAltura += 64;
+
+  for (let index = 0; index < 5; index++) {
+    const numIndex = +index;
+    const posicao = +index + 1;
+
+    const playersNome = players.nome[numIndex]
+      ? players.nome[numIndex].padStart(3, " ")
+      : "???";
+    let playersPonto = players.ponto[numIndex]
+      ? players.ponto[numIndex].toString().padStart(2, "0")
+      : "??";
+    if (playersNome !== "???" && playersPonto === "??") playersPonto = "00";
+    const playersTime = players.time[numIndex]
+      ? players.time[numIndex]
+      : "??:??";
+
+    setTimeout(() => {
+      ctx.fillText(
+        `${posicao} ${playersNome} ${playersPonto} ${playersTime}`,
+        centro.lado,
+        centro.altura + countAltura
+      );
+      countAltura += 40;
+    }, countTempo * numIndex);
+
+    countTempo = 500;
+  }
+}
+
+function cronometroScreen() {
+  let element = document.querySelector(".cronometro");
+  let countFn = setInterval(() => {
+    if (noJogo) {
+      cronometro();
+    } else {
+    }
+
+    minFormatado = countCronometro.min.toString().padStart(2, "0");
+    secFormatado = countCronometro.sec.toString().padStart(2, "0");
+
+    element.textContent = `${minFormatado}:${secFormatado}`;
+  }, 1000);
+
+  function cronometro() {
+    countCronometro.sec--;
+    if (countCronometro.sec < 0) {
+      countCronometro.sec = 60;
+      if (countCronometro.min <= 0) {
+        noJogo = false;
+        ctx.clearRect(0, 0, C_LARGURA, C_ALTURA);
+        ctx.fillRect(0, 0, C_LARGURA, C_ALTURA);
+        fimDeJogo();
+        countCronometro.min = 0;
+        countCronometro.sec = 0;
+        clearInterval(countFn);
+      } else countCronometro.min--;
+    }
+  }
+}
+
+function cicloDeJogo() {
+  if (noJogo) {
+    verificarColisao();
+    mover();
+    ctx.clearRect(0, 0, C_LARGURA, C_ALTURA);
+    ctx.fillRect(0, 0, C_LARGURA, C_ALTURA);
+    fazerDesenho();
+    setTimeout("cicloDeJogo()", ATRASO);
+  } else fimDeJogo();
+}
